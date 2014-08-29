c0af8c7e9ef16df71524c9d5fb09ad21b069bef1
Casey Garland <caseyg1204@gmail.com>
2014-08-29 15:22:41 -0400

    init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('socialSearchApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
