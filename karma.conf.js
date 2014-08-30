6e9c70d2aa1f425d143fe170539dc59396c1666f
Casey Garland <caseyg1204@gmail.com>
2014-08-30 01:36:41 -0400

    coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
     reporters: ['coverage', 'coveralls'],
 
      coverageReporter: {
-    type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+    type: 'lcovonly', // lcov or lcovonly are required for generating lcov.info files
     dir: 'coverage/'
   },
 
