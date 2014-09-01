9e8aaf5aa62e698cf1f3f70695f25893138d7cd5
Justin Cruz <digikid13@gmail.com>
2014-09-01 19:42:31 -0400

    added filter and loading notification

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,8 +2,34 @@
 
 angular.module('socialSearchApp')
   .controller('MainCtrl', function ($scope, $http) {
+    $scope.sites = ['twitter', 'reddit', 'instagram'];
+
+    $scope.updateFilter = function(site) {
+      var i = $.inArray(site, $scope.sites);
+      var $button = $('#' + site + '-button');
+
+      if (i > -1) {
+        $scope.sites.splice(i, 1);
+        $button.removeClass('active');
+      } else {
+        $scope.sites.push(site);
+        $button.addClass('active');
+      }
+    };
+
+    $scope.siteFilter = function (site) {
+      if ($scope.sites.length > 0) {
+        if ($.inArray(site.loc, $scope.sites) < 0) {
+          return;
+        }
+      }
+
+      return site;
+    };
 
     $scope.search = function() {
+      $('#loading-text').slideToggle();
+      $('.cont-media').hide();
       var q = $scope.query.replace(' ', '+');
 
       function shuffle(array) {
@@ -23,14 +49,16 @@ angular.module('socialSearchApp')
         var res = [];
 
         for (var key in data) {
-          data[key].forEach(function(peice) {
-            peice.loc = key;
-            res.push(peice);
+          data[key].forEach(function(post) {
+            post.loc = key;
+            res.push(post);
           });
         }
-
+        setTimeout(function () {
+          $('#loading-text').slideToggle();
+          $('.cont-media').show();
+        }, 2500);
         res = shuffle(res);
-
         $scope.response = res;
       });
       }
