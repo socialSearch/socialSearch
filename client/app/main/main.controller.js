83bbdeab6dd63ec611b689d337e17cd78889188e
Justin Cruz <digikid13@gmail.com>
2014-09-01 12:16:19 -0400

    more stlying and shuffling of posts

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,11 +6,32 @@ angular.module('socialSearchApp')
     $scope.search = function() {
       var q = $scope.query.replace(' ', '+');
 
+      function shuffle(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+           array[i] = array[j];
+          array[j] = temp;
+        }
+        return array;
+      }
+
       if (q) {
       var search = '/api/search?q=' + q;
       
       $http.get(search).success(function(data) {
-        $scope.response = data;
+        var res = [];
+
+        for (var key in data) {
+          data[key].forEach(function(peice) {
+            peice.loc = key;
+            res.push(peice);
+          });
+        }
+
+        res = shuffle(res);
+
+        $scope.response = res;
       });
       }
     };
