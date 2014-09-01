76f1274434de865363cfcf6615256c86d1f920e2
huyphamily <huyphamily@gmail.com>
2014-09-01 11:53:20 -0700

    added error handling for reddit and instagram

--- server/api/search/search.spec.js
+++ server/api/search/search.spec.js
@@ -3,6 +3,7 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var grab = require('./grab.js');
 
 describe('GET /api/search', function() {
 
@@ -16,20 +17,26 @@ describe('GET /api/search', function() {
         res.body.instagram.should.be.instanceof(Array);
         done();
       });
-  }); 
+  });
 
-  // it('should respond with an Array for each Key', function(done) {
-  //   request(app)
-  //     .get('/api/search')
-  //     .expect(200)
-  //     .end(function(err, res) {
-  //       if (err) return done(err);
-  //       res.body.should.be.instanceof(Object);
-  //       done();
-  //     });
-  // });
+  it('should respond with Object with Instagram information', function(done) {
+    
+    grab.instagram('test', function (results){
 
+      results[0].link.should.be.instanceof(String);
+      done();
+    });
 
+  });
 
+  it('should respond with Object with Reddit information', function(done) {
+    
+    grab.reddit('test', function (results){
+
+      results[0].link.should.be.instanceof(String);
+      done();
+    });
+
+  });
 
 });
